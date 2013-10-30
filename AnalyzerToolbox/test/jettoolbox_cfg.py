cacd4e695d8efe4cb48c86a136de43868f5bfbaa
John <jstupak@fnal.gov>
2013-10-29 22:20:39 -0500

    just run Njettiness, produce value map, deswizzle with PAT magic

--- AnalyzerToolbox/test/jettoolbox_cfg.py
+++ AnalyzerToolbox/test/jettoolbox_cfg.py
@@ -1,91 +1,76 @@
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
-# load the PAT config
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
+## uncomment the following line to add different jet collections
+## to the event content
+from PhysicsTools.PatAlgos.tools.jetTools import *
 
-process.source.fileNames = [
-    '/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0076C8E3-9AE1-E111-917C-003048D439AA.root'
-    ]
-
-#-------------------------------------------------------------------------------
-
-from PhysicsTools.PatAlgos.tools.pfTools import *
-
-postfix = "PFlow"
-jetAlgo="AK5"
-usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=True, postfix=postfix)
-
-#-------------------------------------------------------------------------------
-
-from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
-process.goodPatJetsPFlow = cms.EDFilter("PFJetIDSelectionFunctorFilter",
-                                        filterParams = pfJetIDSelector.clone(),
-                                        src = cms.InputTag("selectedPatJetsPFlow")
-                                        )
-process.goodPatJetsCA8PF = cms.EDFilter("PFJetIDSelectionFunctorFilter",
-                                        filterParams = pfJetIDSelector.clone(),
-                                        src = cms.InputTag("selectedPatJetsCA8PF")
-                                        )
-
-#-------------------------------------------------------------------------------
-
-###############################
-###### Bare CA 0.8 jets #######
-###############################
-from RecoJets.JetProducers.ca4PFJets_cfi import ca4PFJets
-process.ca8PFJetsPFlow = ca4PFJets.clone(rParam = cms.double(0.8),
-                                         src = cms.InputTag('pfNoElectron'+postfix),
-                                         doAreaFastjet = cms.bool(True),
-                                         doRhoFastjet = cms.bool(True),
-                                         Rho_EtaMax = cms.double(6.0),
-                                         Ghost_EtaMax = cms.double(7.0)
-                                         )
-
-addJetCollection(process,
-                 cms.InputTag('ca8PFJetsPFlow'),
-                 'CA8', 'PF',
-                 doJTA=True,
-                 doBTagging=True,
-                 doL1Cleaning=False,
-                 doL1Counters=False,
-                 doJetID = False
+# uncomment the following lines to add ak5JPTJets to your PAT output
+addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
+                 'AK5', 'JPT',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 #jetCorrLabel = ('AK5JPT', cms.vstring(['L1Offset', 'L1JPTOffset', 'L2Relative', 'L3Absolute'])),
+                 jetCorrLabel = ('AK5JPT', cms.vstring(['L1Offset', 'L2Relative', 'L3Absolute'])),
+                 doType1MET   = False,
+                 doL1Cleaning = False,
+                 doL1Counters = True,
+                 genJetCollection = cms.InputTag("ak5GenJets"),
+                 doJetID      = True,
+                 jetIdLabel   = "ak5"
                  )
 
+## uncomment the following lines to add ak7CaloJets to your PAT output
+addJetCollection(process,cms.InputTag('ak7CaloJets'),
+                 'AK7', 'Calo',
+                 doJTA        = True,
+                 doBTagging   = False,
+                 jetCorrLabel = ('AK7Calo', cms.vstring(['L1Offset', 'L2Relative', 'L3Absolute'])),
+                 doType1MET   = True,
+                 doL1Cleaning = True,
+                 doL1Counters = False,
+                 genJetCollection=cms.InputTag("ak7GenJets"),
+                 doJetID      = True,
+                 jetIdLabel   = "ak7"
+                 )
+
+## uncomment the following lines to add ak5PFJets to your PAT output
+switchJetCollection(process,cms.InputTag('ak5PFJets'),
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5PF', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute'])),
+                 doType1MET   = True,
+                 genJetCollection=cms.InputTag("ak5GenJets"),
+                 doJetID      = True
+                 )
 
 process.load('JetTools.AnalyzerToolbox.AnalyzerJetToolbox_cff')
-process.Njettiness.src = cms.InputTag("selectedPatJetsCA8PF")
-process.Njettiness.cone = cms.double(0.8)
-process.QGTagger.srcJets = cms.InputTag("selectedPatJetsCA8PF")
-process.QGTagger.useCHS  = cms.untracked.bool(True)
-process.QGTagger.isPatJet = cms.untracked.bool(True)
+process.Njettiness.src = cms.InputTag("ak5PFJets")
+process.Njettiness.cone = cms.double(0.5)
 
-#-------------------------------------------------------------------------------
+process.patJets.userData.userFloats.src = ['Njettiness:tau1','Njettiness:tau2','Njettiness:tau3']
 
-# Let it run
+## let it run
 process.p = cms.Path(
-    getattr(process,"patPF2PATSequence"+postfix)
-    + process.ca8PFJetsPFlow
-    + process.patDefaultSequence
-    + process.goodPatJetsPFlow
-    + process.selectedPatJetsCA8PF
-    + process.AnalyzerJetToolbox
+    process.Njettiness*
+    process.patDefaultSequence
 )
 
-#-------------------------------------------------------------------------------
+process.out.outputCommands = cms.untracked.vstring('keep *')
 
-# Add PF2PAT output to the created file
-from PhysicsTools.PatAlgos.patEventContent_cff import patEventContentNoCleaning
-#process.load("CommonTools.ParticleFlow.PF2PAT_EventContent_cff")
-#process.out.outputCommands =  cms.untracked.vstring('drop *')
-process.out.outputCommands = cms.untracked.vstring('drop *',
-                                                   'keep *_selectedPatJetsCA8PF_*_*',
-                                                   'keep *_Njettiness_tau*_*',
-                                                   'keep *_QGTagger_*_*'
-                                                   )
-
-#-------------------------------------------------------------------------------
-
-process.GlobalTag.globaltag =  'START53_V7E::All'    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
-process.maxEvents.input = 10
+## ------------------------------------------------------
+#  In addition you usually want to change the following
+#  parameters:
+## ------------------------------------------------------
+#
+#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
+#                                         ##
+process.source.fileNames =  ['/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0076C8E3-9AE1-E111-917C-003048D439AA.root']
+#                                         ##
+process.maxEvents.input = 2
+#                                         ##
+#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
+#                                         ##
 process.out.fileName = 'jettoolbox.root'
+#                                         ##
+#   process.options.wantSummary = False   ##  (to suppress the long output at the end of the job)
