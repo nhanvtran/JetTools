d59464ec780dc409e450b2ac8a91e34c1489fcce
John <jstupak@fnal.gov>
2013-11-26 20:28:31 +0100

    minor change

---
+++ AnalyzerToolbox/test/jetanalyzer_cfg.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        'file:jettoolbox.root'
+    )
+)
+
+process.demo = cms.EDAnalyzer('jetAnalyzer',
+                              src=cms.InputTag("selectedPatJets")
+)
+
+
+process.p = cms.Path(process.demo)
